3ca6141099cfde1b41682c3b214dcd87f8712cf6
Nick Nicker <nick4nicker@gmail.com>
2024-08-13 12:41:12 +0200

    Add motors example and enable camera data reading example

--- firmware/car/targets/MKV/source/main.cpp
+++ firmware/car/targets/MKV/source/main.cpp
@@ -19,27 +19,38 @@ int main() {
 
     kitty.display.print(1234);
 
-    
 
-    uint16_t dataBuf[128] = {0};
-    NXP_Camera::Type type = NXP_Camera::Type::CAMERA_1;
-    size_t i = 0;
+    uint16_t         dataBuf[128] = {0};
+    NXP_Camera::Type type         = NXP_Camera::Type::CAMERA_1;
+    size_t           i            = 0;
 
 
     kitty.servo.set(-1.0);
 
+    // Ustawia obydwa silniki na 10% mocy do przodu (zakres od -1.0 do 1.0, podobnie jak w servo)
+    kitty.motors.setValue(0.1, 0.1);
+    log_notice("left: %d, right: %d", kitty.motors.getValue().first, kitty.motors.getValue().second);
+
+    // motors.run() chyba włącza silniki, a motors.block() wyłącza.
+    kitty.motors.run();
+
+    // Po 10s wyłączamy silniki
+    delay_ms(10000);
+    kitty.motors.block();
+
     while (true) {
         kitty.proc();
 
-    //kitty.algorithmUnit.analyze();
-        //kitty.camera.getData(type, dataBuf);
+        // Resztki starego algorytmu do kamery, tutaj tylko odczyt danych
+        kitty.algorithmUnit.analyze();
+        kitty.camera.getData(type, dataBuf);
 
-        /*delay_ms(1000);
-        // log_notice("%d", dataBuf[i%128]);
+        delay_ms(1000);
+        log_notice("%d", dataBuf[i % 128]);
 
-        for (int j = 0; j<128; j++) {
+        for (int j = 0; j < 128; j++) {
             delay_ms(1);
-            log_notice("%d",dataBuf[j]);
-        }*/
+            log_notice("%d", dataBuf[j]);
+        }
     }
 }
