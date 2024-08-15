97062ebdf29e85ca1f6f0ce6bcb948675eb4ec30
Nick Nicker <nick4nicker@gmail.com>
2024-08-15 21:40:15 +0200

    Remove pixy

--- firmware/pixy/g_extract/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-filename=$1
-if [ -n "$filename" ]; then
-    ./extract_green $filename.ppm && ./get_edges2 g_$filename.ppm && convert g_$filename.ppm o$filename.png \
-        && rm -r *_$filename.ppm green_bytes_only
-else
-    echo "filename was not given"
-fi
