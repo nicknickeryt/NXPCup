cf0a1b8cf36ad84241791ffa7e9f59de29107054
Dominik Michalczyk <dmichalczyk73@gmail.com>
2025-03-19 13:30:34 +0100

    stable

---
+++ firmware/car/targets/MKV/tests/toolchain.cmake
@@ -0,0 +1,29 @@
+set(NAME x86)
+
+include(${CMAKE_CURRENT_LIST_DIR}/requirements.cmake)
+
+FetchContent_MakeAvailable(googletest)
+enable_testing()
+set(ALGORITHMS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/targets/MKV/algorithms)
+
+add_executable(
+  x86
+  # test sources
+  ${CMAKE_CURRENT_LIST_DIR}/algorithm_test.cpp
+  # project sources
+  ${ALGORITHMS_DIR}/algorithm.cpp
+)
+
+target_include_directories(
+  x86
+  PRIVATE
+  ${ALGORITHMS_DIR}
+)
+
+target_link_libraries(
+  x86
+  GTest::gtest_main
+)
+
+include(GoogleTest)
+gtest_discover_tests(x86)
