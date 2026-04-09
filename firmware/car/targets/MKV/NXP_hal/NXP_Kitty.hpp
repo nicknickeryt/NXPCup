92b949b904558ec839fa76321f11262238f3bf74
nicknickeryt <nick4nicker@gmail.com>
2026-04-09 17:22:20 +0200

    Increase camera frequency (now ~247Hz)

--- firmware/car/targets/MKV/NXP_hal/NXP_Kitty.hpp
+++ firmware/car/targets/MKV/NXP_hal/NXP_Kitty.hpp
@@ -121,7 +121,7 @@ class Kitty {
     NXP_DMA uart0DMA = {kDmaRequestMux0UART0Tx};
 
     // PIT
-    NXP_PIT pitCamera         = {NXP_PIT::CHANNEL::_0, 55000, NXP_Camera::pitInterruptStatic, nullptr};
+    NXP_PIT pitCamera         = {NXP_PIT::CHANNEL::_0, 65000, NXP_Camera::pitInterruptStatic, nullptr};
     NXP_PIT pitSendCameraData = {NXP_PIT::CHANNEL::_1, 30, pit_generalHandler, nullptr};
     NXP_PIT encodersPit       = {NXP_PIT::CHANNEL::_2, 50, nullptr, nullptr};
 
